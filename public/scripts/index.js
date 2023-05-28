afb0800697b36fc57080dcb19b9764ad67052f9d
Zackaria Antar <antarzack@gmail.com>
2023-05-28 13:50:42 -0500

    changed file structure and file names, started to structure functionality.

---
+++ public/scripts/index.js
@@ -0,0 +1,58 @@
+$(document).ready(onReady)
+let pageData = [
+	{
+		name: "amyrobson",
+		pic: "./images/avatars/image-amyrobson.png",
+		votes: 12,
+		comment:
+			"Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+	},
+	{
+		name: "maxblagun",
+		pic: "./images/avatars/image-maxblagun.png",
+		votes: 5,
+		comment:
+			"Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+	},
+	{
+		name: "ramsesmiron",
+		pic: "./images/avatars/image-ramsesmiron.png",
+		votes: 4,
+		comment:
+			"@maxblagun If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+	},
+	{
+		name: "juliusomo",
+		pic: "./images/avatars/image-juliusomo.png",
+		votes: 2,
+		comment:
+			"@ramsesmiron I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+	},
+];
+
+function onReady(){
+    $('article')
+}
+
+
+function changeVotes(){
+
+}
+
+function addComment(){
+
+}
+
+function editComment() {
+
+}
+
+function removeComment() {
+
+}
+
+function conditionalRender(){
+
+}
+
+
